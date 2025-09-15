729e42ce8aac9bb1cfb31151cab53bfb29cacb8a
LFYStch <lfystch01@gmail.com>
2025-09-15 17:26:35 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -193,9 +193,9 @@ class spawner {
     
     public mesh LFYS(double x, double y, double z, int aI, double theta, double psi) {
     
-    GameObject LFYS = new GameObject(new mesh[]{new mesh(
+    GameObject LFYS = new GameObject(new mesh[]{
         loader.load("Cube.obj",x,y,z)
-    )}, new AABB(new vec3(0, 0, 0, 0, 0), new vec3(0, 0, 0, 0, 0)), theta, psi, x, y, z);
+    }, new AABB(new vec3(0, 0, 0, 0, 0), new vec3(0, 0, 0, 0, 0)), theta, psi, x, y, z);
     return LFYS.getMesh(aI);
 }
 
